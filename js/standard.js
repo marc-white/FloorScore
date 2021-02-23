bc4ca85a9c0861d05a617f5feb1b55dd2fe59597
Marc White <marc.white@anu.edu.au>
2021-02-23 15:57:48 +1100

    Update timings

--- js/standard.js
+++ js/standard.js
@@ -24,8 +24,8 @@ var scoreHome, scoreAway;
 
 // Set global default values
 noPeriods = 3;
-lenPeriod = 14;
-lenBreak = 2;
+lenPeriod = 16;
+lenBreak = 3;
 rollingClock = true;
 
 function resetGame() {
